8c2c638be37622e9467fbd50149c3589a3918ee9
Ahmed <aahmed17@mail.greenriver.edu>
2020-07-17 16:48:50 -0700

    cube function

--- Python/functions.py
+++ Python/functions.py
@@ -7,11 +7,17 @@ def func1():
 # Function that takes an arguement
 def addNums(val1, val2):
     print(val1 + val2)
-
+# function that returns a value
+def cube(x):
+    return x * x * x
 
 # call functions
 #func1()
 #print(func1())
 #print(func1)
 
-addNums (5, 6)
+#addNums (5, 6)
+#addNums(val2 = 11, val1 = 9)
+
+
+print(cube(5))
