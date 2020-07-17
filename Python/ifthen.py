07b89acbf44c621a01563a206c265a7f03c04884
Ahmed <aahmed17@mail.greenriver.edu>
2020-07-16 22:43:00 -0700

    finalized ifthen

--- Python/ifthen.py
+++ Python/ifthen.py
@@ -3,11 +3,13 @@
 
 def main():
 	x = 200
-	y = 100
+	y = 200
 	if x < y: 
 		print("x is less than y")
 	elif x > y:
 		print ("x is greater then y")
-
+	else:
+		print("X is equal to y")
+	
 main()
 
