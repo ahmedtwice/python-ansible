bd5460ee485ebf7c390d0718c94d06210ecada4e
Ahmed <aahmed17@mail.greenriver.edu>
2020-07-17 16:40:43 -0700

    added addNums

--- Python/functions.py
+++ Python/functions.py
@@ -3,11 +3,15 @@
 # Basic function
 
 def func1():
-        print("somehting func-y")
+    print("somehting func-y")
+# Function that takes an arguement
+def addNums(val1, val2):
+    print(val1 + val2)
+
 
 # call functions
+#func1()
+#print(func1())
+#print(func1)
 
-
-func1()
-print(func1())
-print(func1)
+addNums (5, 6)
