fde3d9a772f7756d27456ea40e5c4df7c03093bf
Ahmed <aahmed17@mail.greenriver.edu>
2020-07-17 17:43:12 -0700

    started dictionary

---
+++ Python/dict.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#create dicttionary object
+eng2esp = { "one": "uno", "two" : "dos", "three" : "tres"  }
+
+print(eng2esp)
+
+#view elements
+print(eng2esp["one"])
+print(eng2esp["three"])
+
