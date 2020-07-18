8ccf46b5496ce48e418c04e59a635128b4ee44c9
Ahmed <aahmed17@mail.greenriver.edu>
2020-07-17 18:39:48 -0700

    finalized readingfile

--- Python/readingfile.py
+++ Python/readingfile.py
@@ -7,4 +7,15 @@ testFile = open("testfile.txt", "r" )
 print(testFile.read())
 testFile.close()
 
+# read file line by line
+testFile = open("testfile.txt", "r")
+for line in testFile:
+    if line.startswith("A"):
+        print(line)
+testFile.close()
+
+# Read file using  with statement
+with open("testfile.txt", "r") as testFile:
+    print(testFile.read())
+
 
