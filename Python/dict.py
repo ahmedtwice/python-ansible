2f277e4cac2b79541287710b1afd47731d62f37f
Ahmed <aahmed17@mail.greenriver.edu>
2020-07-17 17:48:05 -0700

    started dictionary

--- Python/dict.py
+++ Python/dict.py
@@ -9,3 +9,8 @@ print(eng2esp)
 print(eng2esp["one"])
 print(eng2esp["three"])
 
+#add and remove items from dictionary
+eng2esp.update({"four":"cuatro", "five":"cinco"})
+eng2esp.pop("two")
+print(eng2esp)
+
