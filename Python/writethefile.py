dce0a5e75983f4be046c12760025a8fadcb0c744
Ahmed <aahmed17@mail.greenriver.edu>
2020-07-17 18:14:23 -0700

    writing files

---
+++ Python/writethefile.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#Example of writing files 
+
+# Open the file
+testFile = open("testfile.txt", "w")
+
+# Write to the a file
+testFile.write("Always look on the bright side of life\n")
+
+# Close the file
+testFile.close()
